ae2b93b36adbeb067e15de50e3f20aeab286f645
HugoBunelier <bunelierpro@gmail.com>
2022-10-24 17:04:50 +0200

    fix

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ sudo apt-get install pip
 pip install yaspin ; pip install datetime ; pip install psutil ; pip install ; pip install simple_term_menu
 sudo pip install yaspin ; sudo pip install datetime ; sudo pip install psutil ; sudo pip install ; sudo pip install simple_term_menu
 clear
-sudo python3 setup.py
+sudo python3 src/setup.py
